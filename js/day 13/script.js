098ba0cf29a585bc6642ddfd36761488bd4fce2a
unknown <shahzadanouman@hotmail.com>
2023-11-01 05:03:31 -0700

    javascript datatypes and variables 1-11-23

---
+++ js/day 13/script.js
@@ -0,0 +1,114 @@
+
+
+/* Output methods   */
+document.write('Hello World! <br>')
+document.write('Hello World! <br> ')
+document.write("<h1> Hello World! </h1> ")
+document.write('<div class="container"> <div class="items">1</div> <div class="items">2</div> <div class="items">3</div> <div class="items">4</div> <div class="items">5</div> <div class="items">9</div> </div>')
+
+/* second method */
+console.log('hello world!')
+console.log('<div class="container"> <div class="items">1</div> <div class="items">2</div> <div class="items">3</div> <div class="items">4</div> <div class="items">5</div> <div class="items">9</div> </div>')
+
+/* third method */
+// alert('hello world!')
+
+
+/*  Data types 
+
+string = '-0-9a-&^%^%$#(*(*&%&%'
+int = 0
+decimal = 0.0
+boolean = true false
+*/
+
+/* 
+ways to assing variables 
+
+let 
+var 
+
+const
+
+*/
+
+
+
+// declare different types of variables
+let a = 'hello World!';
+// alert(a);
+
+a = 'Hello class';
+
+// alert(a);
+
+let b = 1245
+// alert(b)
+
+let c = 1234.332
+// alert(c)
+
+let d = false
+// alert(d)
+
+
+// const variable
+const temp = 12;
+// alert(temp)
+
+// temp = 20
+// alert(temp)
+
+
+let x = ['ahmad','taimoor','muhammad'];
+
+let y = [12,12,25,356,78,995,445,14,554,789,45]
+// alert(x)
+// alert(x[2])
+
+console.log(y[10])
+
+
+
+
+// Arithmetic operators
+let a1 = 11;
+let a2 = 2 ;
+
+let result = a1 + a2;
+
+// console.log(result)
+console.log(a1 + a2)
+console.log(a1 - a2)
+console.log(a1 * a2)
+console.log(a1 / a2)
+console.log(a1 ** a2)
+console.log(a1 % a2)
+
+let y1 = 0;
+console.log(y1)
+y1 = y1 + 1
+console.log(y1)
+y1 = y1 + 1
+console.log(y1)
+
+
+let y2 = 10
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2++
+console.log(y2)
+y2--
+console.log(y2)
+
