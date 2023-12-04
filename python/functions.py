c251ba5341ad13e55a3bca9e8195aabd6f4ad71b
unknown <shahzadanouman@hotmail.com>
2023-12-04 15:56:01 +0500

    py conditions and functions 04-12-23

--- python/functions.py
+++ python/functions.py
@@ -1,3 +1,25 @@
+# Task
+#     Check two given numbers and return true if one of the numbers is 50 or if their sum is 50.
+#     Check from the given integer, whether it is positive or negative.
+#     Check whether a given number is even or odd.
+#     Check whether a given positive number is a multiple of 3.
+#     check from the given two numbers, weather the first number is "greater", "lesser" or "equal" to the second number.
+#     check from the three sides of triangle. use conditions to determine and display 
+#     weather the triangle is "Equilateral" (all sides are equal), "Isosceles" (two sides are equal), or "Scalane" (no sides are equal)
+#     check from the given month (1-12) if its "Winter" (December-Feburary), "Spring" (March-May), "Summer" (June-August), or "Autumn" OR "Fall" (September-November).
+#     Create a function that takes two numbers as input and returns the largest of the two.
+#     create a function that takes two strings as input and returns a new string that concatenates both if them.
+#     Write a function that takes a temperature in celsius and converts it to Fahrenheit.
+#     Write a program that takes the age and checks if they are eligible for voting (e.g 18 or older)
+#     Implement a function that checks if a given number is positive, negative, or zero and return the result.
+#     create a function that takes two numbers as input and returns their product  using arithmetic operations. (e.g the product of 2 and 3 is 6)
+#     implement a function that takes two strings as input and checks if they are equal.
+#     Write a function that takes a number as input and returns the absolute value of the number
+#     Determine whether a given year is a leap year.
+#     All the prior tasks are to be solved with functions.
+
+
+
 print('functions')
 
 # define function
