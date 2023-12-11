92632154d6f557fd7ab23edd8916e11ff0e77c5b
unknown <shahzadanouman@hotmail.com>
2023-12-11 16:10:15 +0500

    flask http request 11-12-23

---
+++ flask/flask-3/index.py
@@ -0,0 +1,53 @@
+from flask import Flask , render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/home/')
+def home():
+    return 'home page'
+
+
+# @app.route('/signup-form')
+@app.route('/signup')
+def signup():
+    # return 'sign up page'
+    # return render_template('signup.html')
+    return render_template('forms/signup.html')
+
+# submit form using POST
+@app.route('/register',methods=['POST'])
+# @app.route('/register',methods=['GET','POST'])
+def register():    
+   
+    form_data = request.form
+    return render_template('register.html',form_data=form_data)
+    
+    # return form_data
+    # first_name = request.form['first_name']
+    # last_name = request.form['last_name']
+    # email = request.form['email']
+    # password = request.form['password']
+    # confirm_password = request.form['confirm_password']
+    # return render_template('register.html',f_name=first_name, last_name=last_name,email=email,password=password,confirm_password=confirm_password )
+    # return f"first Name: {first_name} <br> Last Name: {last_name} <br> Email : {email} <br> Password: {password} <br> Conform Password: {confirm_password}  "
+    # return last_name
+    return 'form is submitted'
+
+# submit form using GET
+@app.route('/register-get')
+def register_get():
+    form_data = request.args
+    # form_data = request.args['first_name']
+    # form_data = request.args.get('first_name')
+    # return form_data
+    return render_template('register.html',form_data=form_data)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
