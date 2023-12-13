15b01135551ecda39420f9b7c734dd37e05dd77f
unknown <shahzadanouman@hotmail.com>
2023-12-13 16:30:10 +0500

    SQL queires 13-12-23

---
+++ SQL/sql_queires.sql
@@ -0,0 +1,44 @@
+-- Create a table of users and perform database queries.
+
+-- get values of specific columns
+SELECT `first_name`,`email` FROM `users`;
+
+-- get value of all columns
+SELECT * FROM `users` ;
+
+-- get values with where clause
+SELECT *  FROM `users` WHERE `email` = 'admin@admin.com';
+
+-- get values by id
+SELECT *  FROM `users` WHERE `id` = 23;
+
+-- get specific columns of desired id
+SELECT `name`,`email`,`password` FROM `users` WHERE `id` = 23;
+
+-- AND example
+SELECT `name`,`email`,`password` FROM `users` WHERE `email` = 'admin@admin.com' AND `id` = 2;
+
+-- OR example
+SELECT `name`,`email`,`password` FROM `users` WHERE `email` = 'admin@admin.com' OR `id` = 23;
+
+-- insert a new record
+INSERT INTO `users` 
+VALUES (1,  'Ahmad', 'Abdullah', 'adhmadabdullah@gmail.com', '+9230012345678');
+
+-- inset new record with columns name
+INSERT INTO `users` (`phone`,`fname`,`lname`,`email`)
+VALUES ('13156564654325','Ahmad','ahsan', 'adhmadahsan@gmail.com');
+
+-- upadate specific row in table
+UPDATE `users`
+SET `phone` = '0300123456789'
+WHERE `id` = 1;
+
+-- second example
+UPDATE `users`
+SET `phone` = '0200123456789'
+WHERE `email` = 'adhmadahsan@gmail.com';
+
+-- Delete row
+DELETE FROM `users` WHERE `id` = 5;
+DELETE FROM `users` WHERE `id` IN (68,71,75,53,60);
---
+++ SQL/users.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2023 at 11:43 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flask-3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'Ahmad', 'Ali', 'ahmadali@gmail.com', 'admin'),
+(2, 'Adeel', 'Muhammad', 'adeelasif@hotmail.com', 'admin'),
+(3, 'ahmad', 'adeel', 'ahmadadeel@gmail.com', 'admin'),
+(4, 'ahmad', 'adeel', 'ahmadadeel@gmail.com', 'admin'),
+(5, 'farrukh', 'Muhammad', 'farrukh@gmail.com', 'admin'),
+(6, 'farrukh', 'Muhammad', 'farrukh@gmail.com', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
