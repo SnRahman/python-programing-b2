b4f70c2de68aa5fe77711254c24be5c4c97769fb
unknown <shahzadanouman@hotmail.com>
2023-12-22 14:35:18 +0500

    flask practise 20-12-12

---
+++ flask/flask-4/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    # return '<h1> this is home page</h1>'
+    return render_template('home.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
